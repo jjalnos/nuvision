5cdb393dbee51aabcfed11ddf2a548f2a01ebf7c
Jeremy J <jalnosjeremy@gmail.com>
2021-07-10 14:22:38 -0500

    Add Angular Materials Library and Dev Dependancies Cleaned Up Pregenerated HTML CSS and TS Began Template For Sean Drane Construction AKA NuVisionCd

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'nuvision';
+  title = 'Sean Drane';
+
+  slides = [
+    {'image': 'https://picsum.photos/seed/picsum/1200/300'},
+    {'image': 'https://picsum.photos/seed/picsum/1200/300'},
+    {'image': 'https://picsum.photos/seed/picsum/1200/300'},
+    {'image': 'https://picsum.photos/seed/picsum/1200/300'},
+    {'image': 'https://picsum.photos/seed/picsum/1200/300'}
+  ];
 }
